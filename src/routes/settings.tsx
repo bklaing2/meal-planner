0ceeafa3b2ec1906311034611df0d89850ae1368
Bryceson Laing <bklaing2@gmail.com>
2026-07-03 17:13:32 -0500

    fetch data in load to fix bug in iOS

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -3,20 +3,23 @@ import { Button } from "@/components/ui/button";
 import { fetchIngredients, fetchMeals } from "@/lib/fetch";
 import { copyToClipboard } from "@/lib/utils";
 
-export const Route = createFileRoute("/settings")({
-  component: RouteComponent,
-});
-
 const fetchData = async () => ({
   ingredients: await fetchIngredients(),
   meals: await fetchMeals(),
 });
 
+export const Route = createFileRoute("/settings")({
+  component: RouteComponent,
+  loader: () => fetchData(),
+});
+
 function RouteComponent() {
+  const data = Route.useLoaderData();
+
   return (
     <Button
       onClick={async () => {
-        await copyToClipboard(JSON.stringify(await fetchData()));
+        await copyToClipboard(JSON.stringify(data));
         alert("Data copied to clipboard as JSON");
       }}
     >
