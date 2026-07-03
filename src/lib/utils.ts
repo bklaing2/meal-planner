c47c32248732898c4809e71bbc35e891cc0f9087
Bryceson Laing <bklaing2@gmail.com>
2026-07-03 17:44:45 -0500

    add "Import data from clipboard button"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import type { Optional } from "@/lib/types";
+import { db } from "@/lib/db";
+import type { Ingredient, Meal, Optional } from "@/lib/types";
+import { createIngredient, createMeal } from "./fetch";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -20,4 +22,17 @@ export function copyToClipboard(text: string) {
     return navigator.clipboard.writeText(text);
 
   return Promise.reject("The Clipboard API is not available.");
+}
+
+export async function importData(json: string) {
+  const { ingredients = [], meals = [] } = JSON.parse(json) as {
+    ingredients: Ingredient[];
+    meals: Meal[];
+  };
+
+  await db.ingredients.clear();
+  await db.meals.clear();
+
+  await db.ingredients.bulkPut(ingredients);
+  await db.meals.bulkPut(meals);
 }
--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import { fetchIngredients, fetchMeals } from "@/lib/fetch";
-import { copyToClipboard } from "@/lib/utils";
+import { copyToClipboard, importData } from "@/lib/utils";
 
 const fetchData = async () => ({
   ingredients: await fetchIngredients(),
@@ -17,13 +17,29 @@ function RouteComponent() {
   const data = Route.useLoaderData();
 
   return (
-    <Button
-      onClick={async () => {
-        await copyToClipboard(JSON.stringify(data));
-        alert("Data copied to clipboard as JSON");
-      }}
-    >
-      Copy data to clipboard as JSON
-    </Button>
+    <>
+      <Button
+        onClick={async () => {
+          await copyToClipboard(JSON.stringify(data));
+          alert("Data copied to clipboard as JSON");
+        }}
+      >
+        Copy data to clipboard as JSON
+      </Button>
+
+      <Button
+        onClick={async () => {
+          try {
+            const json = (await navigator?.clipboard?.readText()) || "";
+            await importData(json);
+            alert("Data imported from clipboard");
+          } catch (error) {
+            alert(`Failed to import data from clipboard: ${error}`);
+          }
+        }}
+      >
+        Import data from clipboard (this will clear existing data)
+      </Button>
+    </>
   );
 }
